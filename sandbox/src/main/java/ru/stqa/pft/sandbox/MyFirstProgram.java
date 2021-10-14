509d2477ac97394ad993799f2b7fc7c7921fcdaa
IIvanov57 <ivanov080892@mail.ru>
2021-10-14 22:28:20 +0300

    Вернул ранее удаленный проект

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -2,10 +2,21 @@ package ru.stqa.pft.sandbox;
 
 public class MyFirstProgram {
     public static void main(String[] args) {
+        hello("world");
+        hello("user");
+        Square s = new Square(5);
+        System.out.println("Площадь квадрата с стороной " + s.l + "=" + s.area());
+
+        Rectangle r = new Rectangle(4, 6);
+        System.out.println("Площадь прямоугольника с сторонами " + r.a + " и " + r.b + "=" + r.area());
 
         Point d = new Point(4,8,2,4);
         System.out.println("Расстояние между двумя точками = " + d.distance());
     }
+    public static void hello(String somebody) {
+        System.out.println("Hello," + somebody+ "!");
+    }
 
 }
+
 
