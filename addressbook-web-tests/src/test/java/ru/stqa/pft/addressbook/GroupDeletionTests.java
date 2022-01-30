6bb066b139681f372630464b56a4cd1da3dcda77
IIvanov57 <ivanov080892@mail.ru>
2022-01-30 15:56:34 +0300

    Доделал GroupDeletionTests с базовым классом

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/GroupDeletionTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/GroupDeletionTests.java
@@ -1,25 +1,18 @@
 package ru.stqa.pft.addressbook;
 
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.NoAlertPresentException;
-import org.openqa.selenium.NoSuchElementException;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.annotations.*;
+import org.testng.annotations.Test;
 
-import java.util.concurrent.TimeUnit;
 
 
 public class GroupDeletionTests extends TestBase{
 
   @Test
   public void testGroupDeletionTests() throws Exception {
-    wd.findElement(By.linkText("groups")).click();
-    wd.findElement(By.name("selected[]")).click();
-    wd.findElement(By.name("delete")).click();
-    wd.findElement(By.linkText("group page")).click();
-    wd.findElement(By.linkText("Logout")).click();
+    gotoGroupPage();
+    selectGroup();
+    deleteSelectedGroups();
+    returnGroupPage();
 
   }
 
