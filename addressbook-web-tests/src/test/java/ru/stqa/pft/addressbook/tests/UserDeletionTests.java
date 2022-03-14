56b822e1e05d9be9085dacc27f225a8a7ad4c55f
IIvanov57 <ivanov080892@mail.ru>
2022-03-15 00:17:31 +0300

    добавил правило для testUserModification

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/UserDeletionTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/UserDeletionTests.java
@@ -5,8 +5,9 @@ import ru.stqa.pft.addressbook.model.UserData;
 
 public class UserDeletionTests extends TestBase {
   @Test
-  public void testUserDeletion(){
-if (! app.getUserHelper().isThereAUser()){
+  public void testUserDeletion() {
+    //если нет контакта для удаления, то создать его
+    if (!app.getUserHelper().isThereAUser()) {
       app.getUserHelper().createUser(new UserData("Ivan", "Ivanov", "OOO", "89997775567", "exampel@ya.ru", "test1"));
     }
     app.getUserHelper().selectUser();
--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/UserModificationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/UserModificationTests.java
@@ -6,9 +6,13 @@ import ru.stqa.pft.addressbook.model.UserData;
 public class UserModificationTests extends TestBase {
   @Test
   public void testUserModification() {
+    //если нет контака для модификации, то создать его
+    if (!app.getUserHelper().isThereAUser()) {
+      app.getUserHelper().createUser(new UserData("Ivan", "Ivanov", "OOO", "89997775567", "exampel@ya.ru", "test1"));
+    }
     app.getUserHelper().selectUser();
     app.getUserHelper().initUserModification();
-    app.getUserHelper().fillUser(new UserData("Ivan111", "Ivanov", "OOO", "89997775567", "exampel@ya.ru", null),false);
+    app.getUserHelper().fillUser(new UserData("Ivan111", "Ivanov", "OOO", "89997775567", "exampel@ya.ru", null), false);
     app.getUserHelper().submitUserModification();
     app.getUserHelper().gotoHomePage();
 
