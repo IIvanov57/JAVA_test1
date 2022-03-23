19822b19fb55762481df5d5cb6b94323a4f6f7cc
IIvanov57 <ivanov080892@mail.ru>
2022-03-23 23:36:01 +0300

    Формирование коллекции

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
@@ -4,7 +4,8 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.GroupData;
 
-import static org.testng.Assert.assertEquals;
+import java.util.List;
+
 
 
 public class GroupCreationTests extends TestBase {
@@ -12,10 +13,10 @@ public class GroupCreationTests extends TestBase {
   @Test
   public void testGroupCreation() throws Exception {
     app.getNavigationHelper().gotoGroupPage();
-    int before = app.getGroupHelper().getGroupCount();
+    List<GroupData> before = app.getGroupHelper().getGroupList();
     app.getGroupHelper().createGroup(new GroupData("test2", null, null));
-    int after = app.getGroupHelper().getGroupCount();
-    Assert.assertEquals(after ,before + 1);
+    List<GroupData> after = app.getGroupHelper().getGroupList();
+    Assert.assertEquals(after.size() ,before.size() + 1);
   }
 
 }
